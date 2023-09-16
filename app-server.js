9ec2a65801f8ad531b7f9113fb296e2bfd4505ea
bigshu13 <bigshu13@gmail.com>
2023-09-15 20:23:52 -0400

    change ports to 3000

--- app-server.js
+++ app-server.js
@@ -6,7 +6,7 @@ const { getPexelsData } = require('./pexel-server');
 const cors = require('cors');
 
 const corsOptions = {
-	origin: 'http://localhost:3003'
+	origin: 'http://localhost:3000'
   };
   
   /* Middleware */
